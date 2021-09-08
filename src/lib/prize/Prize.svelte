4f49cfe79900ba093507687d50114c3f6e8b2303
Freddy Silber <freddy.silber@cloudcoach.com>
2021-09-08 12:52:39 -0600

    init

---
+++ src/lib/prize/Prize.svelte
@@ -0,0 +1,11 @@
+<script>
+	const amount = 100;
+</script>
+
+<h1>Prize: ${amount} Gift Card!</h1>
+
+<style>
+	h1 {
+		color: #686868;
+	}
+</style>
---
+++ src/routes/game/index.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import Prize from "$lib/prize/Prize.svelte";
+
+	let weeks: number[] = [...Array(19).keys()];
+	let players = ["Freddy", "Lydia", "Jay", "Carmen"];
+</script>
+
+<div class="game-container">
+	<Prize />
+
+	<table>
+		<tr>
+			<th />
+			{#each weeks as week}
+				<th>Week {week + 1}</th>
+			{/each}
+		</tr>
+		{#each players as player}
+			<tr>
+				<td>{player}</td>
+				{#each weeks as week}
+					<td>{week + 1}</td>
+				{/each}
+			</tr>
+		{/each}
+	</table>
+</div>
+
+<style>
+	div.game-container {
+		border: 1px solid black;
+		border-radius: 5px;
+	}
+
+	table {
+		font-family: arial, sans-serif;
+		border-collapse: collapse;
+		width: 100%;
+	}
+
+	td,
+	th {
+		border: 1px solid #dddddd;
+		text-align: left;
+		padding: 8px;
+	}
+
+	tr:nth-child(even) {
+		background-color: #dddddd;
+	}
+</style>
