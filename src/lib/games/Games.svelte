4a8c1fc8edbed887128cb850692198f00ad08627
freddysilber <freddy.silber@gmail.com>
2022-01-02 21:53:01 -0700

    fixed

--- src/lib/games/Games.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { Games } from '../../models/game';
-
-	export let games: Games;
-</script>
-
-<p>Games</p>
-<p>{games.length}</p>
--- src/routes/games/index.svelte
+++ src/routes/games/index.svelte
@@ -1,48 +1,48 @@
 <script lang="ts">
 	import { getStores } from '$app/stores';
 	import { ENV } from '$lib/env';
-	import Games from '$lib/games/Games.svelte';
 	import axios from 'axios';
 	import Collapsible from 'spaper/components/Collapsible.svelte';
-	import { onMount } from 'svelte';
 	import type { Game } from '../../models/game';
 
 	let games: Game[] = [];
 
 	const { session } = getStores();
 
-	// onMount(()=>{
-	// 	console.log($session)
-	// 	axios
-	// 		.get(`${ENV.api}/users/games/${$session.profile.id}`)
-	// 		.then((response) => {
-	// 			console.log(response);
-	// 			games = response.data;
-	// 			console.log(games);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.error(error);
-	// 		});
-	// });
+	interface AuthProfile {
+		id: number;
+		name: string;
+		username: string;
+		password: string;
+		roles: string[];
+	}
 
-	// session.subscribe((session) => {
-	// 	console.log(session);
-	// 	axios
-	// 		.get(`${ENV.api}/users/games/${session.profile.id}`)
-	// 		.then((response) => {
-	// 			console.log(response);
-	// 			games = response.data;
-	// 			console.log(games);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.error(error);
-	// 		});
-	// });
+	interface Auth {
+		authenticated: boolean;
+		profile: AuthProfile;
+	}
+
+	session.subscribe((session: Auth) => {
+		axios
+			.get(`${ENV.api}/users/games/${session.profile.id}`)
+			.then((response) => {
+				games = response.data.games;
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	});
 </script>
 
 <h1>My Games</h1>
-<!-- <Games {games} /> -->
+<Collapsible label="Games I Own">
+	<ol>
+		{#each games as game}
+			<li>{game.name} - {game.description}</li>
+		{/each}
+	</ol>
+</Collapsible>
 
 <Collapsible label="Games I Joined">
-	I have a bowling personal best of 194
+	<h4>Games I Joined</h4>
 </Collapsible>
