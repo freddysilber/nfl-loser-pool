4e68b75eb116e0dc5084c66e428c8f3cdd3d9775
freddysilber <freddy.silber@gmail.com>
2022-01-02 23:47:06 -0700

    .

--- src/routes/games/index.svelte
+++ src/routes/games/index.svelte
@@ -5,7 +5,9 @@
 	import Collapsible from 'spaper/components/Collapsible.svelte';
 	import type { Game } from '../../models/game';
 
-	let games: Game[] = [];
+	let ownedGames: Game[] = [];
+	let allGames: Game[] = [];
+	let joinedGames: Game[] = [];
 
 	const { session } = getStores();
 
@@ -24,25 +26,57 @@
 
 	session.subscribe((session: Auth) => {
 		axios
-			.get(`${ENV.api}/users/games/${session.profile.id}`)
+			.get(`${ENV.api}/users/games/${session.profile.id}`, {
+				withCredentials: true,
+			})
 			.then((response) => {
-				games = response.data.games;
+				ownedGames = response.data.games;
 			})
 			.catch((error) => {
 				console.error(error);
 			});
+
+		axios
+			.get(`${ENV.api}/games`, { withCredentials: true })
+			.then((response) => {
+				allGames = response.data.games;
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+
+		// axios
+		// 	.get(`${ENV.api}/players/?player=${session.profile.id}`, { withCredentials: true })
+		// 	.then((response) => {
+		// 		joinedGames = response.data.games;
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error(error);
+		// 	});
 	});
 </script>
 
 <h1>My Games</h1>
 <Collapsible label="Games I Own">
 	<ol>
-		{#each games as game}
+		{#each ownedGames as game}
 			<li>{game.name} - {game.description}</li>
 		{/each}
 	</ol>
 </Collapsible>
 
-<Collapsible label="Games I Joined">
-	<h4>Games I Joined</h4>
+<Collapsible label="All Games">
+	<ol>
+		{#each allGames as game}
+			<li>{game.name} - {game.description}</li>
+		{/each}
+	</ol>
+</Collapsible>
+
+<Collapsible label="Joined Games">
+	<ol>
+		{#each joinedGames as game}
+			<li>{game.name} - {game.description}</li>
+		{/each}
+	</ol>
 </Collapsible>
