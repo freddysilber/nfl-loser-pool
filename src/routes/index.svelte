19b480d0e3ff43962c1c496e09bea4d5b6dc55da
Freddy Silber <freddy.silber@cloudcoach.com>
2022-03-19 21:44:32 -0600

    updates schedule link UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import {Button} from 'svelte-materialify';
+	// This is a link we need to update on a per season basis
 	const SCHEDULE_URL = `https://www.google.com/search?q=nfl+schedule&rlz=1C1CHBF_enUS851US854&oq=nfl+schedule&aqs=chrome.0.69i59i131i433i512j0i131i433i512l4j0i433i512j0i131i433i512l2j0i433i512j0i131i433i512.2480j1j4&sourceid=chrome&ie=UTF-8#sie=lg;/g/11nym9rnk6;6;/m/059yj;mt;fp;1;;`;
+
+	function goToSchedule(): void {
+		window.open(SCHEDULE_URL);
+	}
 </script>
 
 <svelte:head>
@@ -9,10 +14,13 @@
 
 <section>
 	<h1 class="text-h1 graduate-text">NFL Loser Pool!</h1>
-	<Button text class="primary-text graduate-text">Schedule</Button>
-	<!-- <h6 class="text-h6 graduate-text">
-		<a href={SCHEDULE_URL} target="_blank">Schedule</a>
-	</h6> -->
+	<Button
+		text
+		class="graduate-text"
+		on:click={goToSchedule}
+	>
+		Schedule
+	</Button>
 </section>
 
 <style lang="scss">
