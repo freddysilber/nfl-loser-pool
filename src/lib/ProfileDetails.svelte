820de5c8057ae73da656187e22eb472e21ce081f
Freddy Silber <freddy.silber@cloudcoach.com>
2022-03-26 01:16:21 -0600

    .

--- src/lib/ProfileDetails.svelte
+++ src/lib/ProfileDetails.svelte
@@ -4,10 +4,18 @@
 	export let user: User;
 </script>
 
-<div>Manage your profile</div>
-<p class="white-text">Id: {user.id}</p>
-<p class="white-text">Name: {user.name}</p>
-<p class="white-text">Username: {user.username}</p>
-<p class="white-text">
-	Account Created: {new Date(user.createdAt).toLocaleDateString()}
-</p>
+<div class="details">
+	<div class="pink-text mb-2">Manage your profile</div>
+	<p class="white-text">Id: {user.id}</p>
+	<p class="white-text">Name: {user.name}</p>
+	<p class="white-text">Username: {user.username}</p>
+	<p class="white-text">
+		Account Created: {new Date(user.createdAt).toLocaleDateString()}
+	</p>
+</div>
+
+<style lang="scss">
+	.details {
+		margin: 1rem;
+	}
+</style>
