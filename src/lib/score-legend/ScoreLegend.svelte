dbaec44cf97a11aca72e6e1b153e926dfd430c8e
Freddy Silber <freddy.silber@cloudcoach.com>
2022-04-22 22:21:07 -0600

    legend UI work

--- src/lib/score-legend/ScoreLegend.svelte
+++ src/lib/score-legend/ScoreLegend.svelte
@@ -2,25 +2,44 @@
 	import { Tooltip } from 'svelte-materialify';
 
 	const legendItems = [
-		{ class: 'zero', content: '0 Points - Guess was was incorrect or was a duplicate guess' },
+		{
+			class: 'pending',
+			content: `You haven't voted yet!`,
+		},
+		{
+			class: 'zero',
+			content:
+				'0 Points - Guess was was incorrect or was a duplicate guess',
+		},
 		{ class: 'half', content: '1/2 Point - Guess resulted in a tie' },
-		{ class: 'one', content: '1 Point - Guessed the losing team but the wrong point spread' },
-		{ class: 'two', content: '2 Points - Guessed the losing team and point spread correctly!' },
+		{
+			class: 'one',
+			content:
+				'1 Point - Guessed the losing team but the wrong point spread',
+		},
+		{
+			class: 'two',
+			content:
+				'2 Points - Guessed the losing team and point spread correctly!',
+		},
 	];
 </script>
 
 <div class="d-flex flex-column justify-center">
 	<div class="d-flex">
 		{#each legendItems as item}
-		<Tooltip top>
-			<div class="{item.class} legend-item"></div>
-			<span slot="tip">{item.content}</span>
-		</Tooltip>
+			<Tooltip top>
+				<div class="{item.class} legend-item" />
+				<span slot="tip">{item.content}</span>
+			</Tooltip>
 		{/each}
 	</div>
 </div>
 
 <style>
+	.pending {
+		background: gray;
+	}
 	.zero {
 		background: red;
 	}
